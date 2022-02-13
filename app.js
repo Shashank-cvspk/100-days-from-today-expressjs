c3bebe64b2b9b7dc235afd3ec441769578807c12
Shashank-cvspk <shashankcvspk@gmail.com>
2022-02-13 13:58:50 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const addDays = require("date-fns/addDays");
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  const todayDate = new Date();
+  const result = addDays(
+    new Date(
+      todayDate.getFullYear(),
+      todayDate.getMonth(),
+      todayDate.getDate()
+    ),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+app.listen(3000);
+
+module.exports = app;
